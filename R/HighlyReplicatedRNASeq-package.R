b8be9a0a0bc1f2025812efb242586d97249bf74b
const-ae <artjom31415@googlemail.com>
2020-03-26 12:44:11 +0100

    Add example to Schurch16()

--- R/HighlyReplicatedRNASeq-package.R
+++ R/HighlyReplicatedRNASeq-package.R
@@ -1,4 +1,23 @@
 
+#' HighlyReplicatedRNASeq: Collection of Bulk RNA-Seq Experiments With Many Replicates
+#'
+#' The HighlyReplicatedRNASeq package provides access to the count matrix results
+#' from studies with many replicates. These datasets can be valuable for
+#' benchmarking tools designed to handle RNA-seq data.
+#'
+#' Available datasets:
+#'
+#' + Schurch et al. (2016): 86 samples of S. *cerevisiae* in two conditions
+#'   - [Schurch16()] / [Schurch16_metadata()]
+#'
+#'
+#' At the moment, this package contains only one dataset, but more
+#' datasets can be added in the future.
+#'
+#' @docType package
+#' @name HighlyReplicatedRNASeq
+NULL
+
 
 #' @importFrom ExperimentHub ExperimentHub
 NULL
